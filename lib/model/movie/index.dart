fb97f6417a1f804d711d769e2f65661344ed85a4
sezenteen <sezenteen@gmail.com>
2024-10-17 13:36:04 +0800

    jsontoi ajillah

--- lib/model/movie/index.dart
+++ lib/model/movie/index.dart
@@ -1,3 +1,25 @@
 import 'package:json_annotation/json_annotation.dart';
 
-class MovieModel {}
+part 'index.g.dart';
+
+@JsonSerializable()
+class MovieModel {
+  int id;
+  String imgUrl;
+  int? publishedYear, durationMin;
+  String? type, description;
+
+  MovieModel({
+    required this.id,
+    required this.imgUrl,
+    this.description,
+    this.durationMin,
+    this.publishedYear,
+    this.type,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) =>
+      _$MovieModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MovieModelToJson(this);
+}
