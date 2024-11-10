4861456872eb5b1d1a670bb761f09143900bee0c
sezenteen <sezenteen@gmail.com>
2024-11-10 19:05:39 +0800

    44-r hiceel zassan

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -4,10 +4,19 @@ import 'package:movie_app/model/movie/index.dart';
 class MovieCard extends StatelessWidget {
   final MovieModel data;
 
-  const MovieCard(this.data , {super.key});
+  const MovieCard(this.data, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final width = MediaQuery.of(context).size.width * 0.3;
+    return Column(
+      children: [
+        Container(
+          height: 200,
+          width: 100,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage(data.imgUrl))),
+      )
+    ]);
   }
 }
